9dc8330064a3b88965dc1cc3b5da824f7b588395
joaorjribeiro <joaorjribeirobr@gmail.com>
2026-04-04 19:58:15 -0300

    Force TCP connection

--- includes/config.php
+++ includes/config.php
@@ -6,7 +6,10 @@ $dbname = getenv('MYSQLDATABASE') ?: 'invicta_financas';
 $port = getenv('MYSQLPORT') ?: '3306';
 
 try {
-    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $user, $pass);
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8;", $user, $pass, [
+        PDO::ATTR_TIMEOUT => 10,
+        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
+    ]);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Erro na conexão: " . $e->getMessage());
