b805d7c9a781c2b0ecacb5bc85fd506c379e03ba
joaorjribeiro <joaorjribeirobr@gmail.com>
2026-04-04 21:55:21 -0300

    config test

--- includes/config.php
+++ includes/config.php
@@ -1,17 +1,20 @@
 <?php
 
-$host = getenv('MYSQLHOST') ?: 'localhost';
-$user = getenv('MYSQLUSER') ?: 'root';
-$pass = getenv('MYSQLPASSWORD') ?: '';
-$dbname = getenv('MYSQLDATABASE') ?: 'invicta_financas';
-$port = getenv('MYSQLPORT') ?: '3306';
+$host = 'junction.proxy.rlwy.net';
+$port = '14900';
+$user = 'root';
+$pass = 'GxMSPpuyVgMTVTlImAtanQaVaWCRWrac';
+$dbname = 'railway';
 
 try {
-    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8;", $user, $pass, [
-        PDO::ATTR_TIMEOUT => 10,
-        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
-    ]);
+    $pdo = new PDO(
+        "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8;",
+        $user,
+        $pass
+    );
+
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
 } catch (PDOException $e) {
     die("Erro na conexão: " . $e->getMessage());
 }
